7fdb753174880cbd04e70c4cdb7f03f109eb4e92
khalid-jobs <qinkai1994@qq.com>
2021-09-30 15:12:03 +0800

    copy from https://github.com/kubernetes-sigs/kubebuilder/tree/master/docs/book/src/cronjob-tutorial/testdata/project

--- cronjob/go.mod
+++ cronjob/go.mod
@@ -1,11 +1,13 @@
-module my.domain/cronjob
+module tutorial.kubebuilder.io/project
 
 go 1.16
 
 require (
-	github.com/onsi/ginkgo v1.14.1
-	github.com/onsi/gomega v1.10.2
-	k8s.io/apimachinery v0.20.2
-	k8s.io/client-go v0.20.2
-	sigs.k8s.io/controller-runtime v0.8.3
+	github.com/onsi/ginkgo v1.16.4
+	github.com/onsi/gomega v1.13.0
+	github.com/robfig/cron v1.2.0
+	k8s.io/api v0.21.2
+	k8s.io/apimachinery v0.21.2
+	k8s.io/client-go v0.21.2
+	sigs.k8s.io/controller-runtime v0.9.2
 )
--- cronjob/main.go
+++ cronjob/main.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021.
+Copyright 2021 The Kubernetes authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+// +kubebuilder:docs-gen:collapse=Apache License
 
 package main
 
@@ -31,11 +32,21 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	batchv1 "my.domain/cronjob/api/v1"
-	"my.domain/cronjob/controllers"
+	batchv1 "tutorial.kubebuilder.io/project/api/v1"
+	"tutorial.kubebuilder.io/project/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
+// +kubebuilder:docs-gen:collapse=Imports
+
+/*
+The first difference to notice is that kubebuilder has added the new API
+group's package (`batchv1`) to our scheme.  This means that we can use those
+objects in our controller.
+
+If we would be using any other CRD we would have to add their scheme the same way.
+Builtin types such as Job have their scheme added by `clientgoscheme`.
+*/
 var (
 	scheme   = runtime.NewScheme()
 	setupLog = ctrl.Log.WithName("setup")
@@ -48,7 +59,14 @@ func init() {
 	//+kubebuilder:scaffold:scheme
 }
 
+/*
+The other thing that's changed is that kubebuilder has added a block calling our
+CronJob controller's `SetupWithManager` method.
+*/
+
 func main() {
+	/*
+	 */
 	var metricsAddr string
 	var enableLeaderElection bool
 	var probeAddr string
@@ -57,6 +75,7 @@ func main() {
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
+
 	opts := zap.Options{
 		Development: true,
 	}
@@ -71,12 +90,14 @@ func main() {
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "73bab12b.tutorial.kubebuilder.io",
+		LeaderElectionID:       "80807133.tutorial.kubebuilder.io",
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
 	}
+
+	// +kubebuilder:docs-gen:collapse=old stuff
 
 	if err = (&controllers.CronJobReconciler{
 		Client: mgr.GetClient(),
@@ -84,6 +105,21 @@ func main() {
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "CronJob")
 		os.Exit(1)
+	}
+
+	/*
+		We'll also set up webhooks for our type, which we'll talk about next.
+		We just need to add them to the manager.  Since we might want to run
+		the webhooks separately, or not run them when testing our controller
+		locally, we'll put them behind an environment variable.
+
+		We'll just make sure to set `ENABLE_WEBHOOKS=false` when we run locally.
+	*/
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&batchv1.CronJob{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "CronJob")
+			os.Exit(1)
+		}
 	}
 	//+kubebuilder:scaffold:builder
 
@@ -101,4 +137,5 @@ func main() {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
+	// +kubebuilder:docs-gen:collapse=old stuff
 }
